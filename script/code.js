d341d053f641002764a9f319ec9f2217711cd064
ganiefah <ganiefahmajiet3@gmail.com>
2023-05-23 14:02:54 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,31 @@
+//making the bmi function
+function calculateBmi(){
+    let weight = document.getElementById('Weight').value
+    let height = document.getElementById('Height').value
+    let bmi = (weight / ((height * height) /10000)).toFixed(2)
+
+    document.getElementById('display').innerHTML = `Your BMI is : ${bmi}`
+
+    if (bmi <= 24.9) {
+        document.getElementById('message').innerHTML ='You are UNDERWEIGHT'
+    }else if (bmi >= 25 && bmi <= 29.9) {
+
+        document.getElementById('message').innerHTML ='You are NORMAL WEIGHT'
+    }else {
+        document.getElementById('message').innerHTML ='You are OVER WEIGHT'
+    }
+}
+
+
+//clear function
+function clears() {
+  window.location.reload() //will clear all data displayed
+}
+clear.addEventListener("click", () => {
+  clears();
+});
+
+
+
+
+
